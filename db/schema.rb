4e57bbf9630580a945cc36ac6bdbc433deadb5e3
Fan Yang <dianayangfan@hotmail.com>
2024-08-28 12:59:29 +0200

    update migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_27_101941) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_28_105521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_27_101941) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "logo"
+    t.string "contact_info"
+    t.string "description"
   end
 
   create_table "favorites", force: :cascade do |t|
