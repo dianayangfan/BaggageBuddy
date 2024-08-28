a309f538b204c314055af7cecc5fe113aa1d2544
Kathy Kolonics <kata.kolonics@gmail.com>
2024-08-28 14:17:12 +0200

    created basic show page for airlines and its policies (no styling yet)

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -1,2 +1,8 @@
 class AirlinesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
+  def show
+    @airline = Airline.find(params[:id])
+    @policies = Policy.where(airline_id: @airline)
+  end
 end
