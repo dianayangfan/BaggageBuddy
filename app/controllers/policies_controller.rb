66638917d9860e7f16b7d11cbaffa646a1aafbd2
Fan Yang <dianayangfan@hotmail.com>
2024-08-29 14:46:02 +0200

    policy MVC done

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -1,10 +1,10 @@
 class PoliciesController < ApplicationController
 
-    def index
-      @policies = Policy.all
-    end
+  # def index
+  #  @policies = Policy.all
+  # end
 
-    def show
-      @policy = Policy.find(params[:id])
-    end
+  def show
+    @policy = Policy.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :airlines, only: [:show]
-
   resources :users, only: [:edit, :update]
   get 'profile', to: "users#profile"
 
   resources :policies, only: [:show]
-get 'airlines/:id/policies/:id', to: "policies#show", as: :airline_policy
+  get 'airlines/:id/policies/:id', to: "policies#show", as: :airline_policy
 end
