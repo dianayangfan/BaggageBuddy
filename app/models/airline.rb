0190dcf6471c8a2f32265553e2c990b0230f2f6d
Fan Yang <dianayangfan@hotmail.com>
2024-08-30 15:56:59 +0200

    rails db migrate

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,4 +1,4 @@
 class Airline < ApplicationRecord
   has_one_attached :logo
-  has_many :policies
+  has_many :policies, dependent: :destroy
 end
--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,3 +1,4 @@
 class Policy < ApplicationRecord
   belongs_to :airline
+  has_many :favorites, dependent: :destroy
 end
